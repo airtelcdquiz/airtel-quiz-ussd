0db8e5d4bb75aebfb3d08fa724d64ead26f3d51d
airtelcdquiz <airtel.cd.quiz@gmail.com>
2026-02-11 20:42:29 +0000

    catch url error

--- src/services/submitService.js
+++ src/services/submitService.js
@@ -3,7 +3,7 @@ const { api } = require("../utils/api");
 async function submit(url, data) {
   try {
     if (typeof url !== "string") {
-      throw new Error("URL must be a string")
+      throw new Error("URL must be a string : " + url);
     }
 
     console.log("Submitting to:", url, "data:", data);
