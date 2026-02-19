c17c9a65e7f5f06fe42c44e40a27d4b36e071d1b
Yvon Flour Alvin <me@yvonflour.com>
2026-02-19 11:14:56 +0000

    try to fixe redis

--- src/redis.js
+++ src/redis.js
@@ -3,6 +3,17 @@ const Redis = require("ioredis");
 const redis = new Redis({
   host: "ussd-redis",
   port: 6379,
+  retryStrategy(times) {
+    return Math.min(times * 200, 2000);
+  },
+});
+
+redis.on("error", (err) => {
+  console.error("Redis error:", err.message);
+});
+
+redis.on("connect", () => {
+  console.log("Connected to Redis");
 });
 
 module.exports = redis;
