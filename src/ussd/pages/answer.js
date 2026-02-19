d1fab65dad3eaba9125d2a8a6de37a79b7cf5edd
Yvon Flour Alvin <me@yvonflour.com>
2026-02-19 19:39:41 +0000

    fixe

--- src/ussd/pages/answer.js
+++ src/ussd/pages/answer.js
@@ -7,31 +7,71 @@ module.exports = {
     // PAGE D'ACCUEIL
     ANSWER: {
         step: "ANSWER",
-        text: "Repondre à la question :",
-        saveAs: "response",
-        nextStep:  "END_ANSWER" ,
+        text: "Vous droit à une seul tentative de pour consulter et repondre à la question ! \n1. Continuer\n2. Plus tard",
+        saveAs: "no",
+        nextSteps: {
+            "1": "CONTINUE_ANSWER",
+            "2": "ANSWER_AFTER"
+        }  ,
         end: false
+    },
+    CONTINUE_ANSWER: {
+        step: "CONTINUE_ANSWER",
+        handler: async (session, input) => {
+
+            return {
+                step: "CONTINUE_ANSWER",
+                text: `${session.sessionData.question_details.question}\n1. Suivant`,
+                nextStep: "QUESTION_OPTIONS",
+                end: false
+            }
+        }
+    },
+    QUESTION_OPTIONS: {
+        step: "QUESTION_OPTIONS",
+        text: `Options :\n1. ${session.sessionData.question_details.option_1}\n2. ${session.sessionData.question_details.option_2}\n3. ${session.sessionData.question_details.option_3}\n4. ${session.sessionData.question_details.option_4}`,
+        saveAs: 'response',
+        nextStep: "QUESTION_ANSWER",
+        end: false
+    },
+    QUESTION_ANSWER: {
+        step: "QUESTION_ANSWER",
+        handler: async (session, input) => {
+            if(session.sessionData.question_details.response == input){
+                return {
+                    step: "QUESTION_ANSWER",
+                    text: `Félicitation !! Vous avez fourni la bonne reponse !`,
+                    nextStep: "QUESTION_ANSWER",
+                    end: true
+                }
+            }else{
+                return {
+                    step: "QUESTION_ANSWER",
+                    text: `Désolé !! Vous n'avez fourni la bonne reponse`,
+                    nextStep: "QUESTION_ANSWER",
+                    end: true
+                }
+            }
+        }
     },
      // PAGE D'ACCUEIL
     END_ANSWER: {
         step: "END_ANSWER",
         handler: async (session, input) => {
-            // Enregistrer la réponse de l'utilisateur
-            session.data.response = input.trim();
-            if (!session.data.response || session.data.response < 0 || session.data.response > 4) {
-                return {
-                    text: "Aucune réponse saisie ou réponse invalide. Veuillez réessayer.",
-                    nextStep: "ANSWER",
-                    end: false
-                };
-            }
             return { 
-                text: "Merci d'avoir participé au quiz ! Votre réponse a été enregistrée.", 
+                text: "Merci d'avoir participé au quiz !", 
                 nextStep: "END_APPLICATION",
                 url:"http://quiz-user-service:3000/api/answers/dayly",
                 end: true 
             };
         },
     },
+    ANSWER_AFTER: {
+        step: "ANSWER_AFTER",
+        text: "N'oubliez pas de revenir repondre à la question du jour avan 23:59 !!", 
+        nextStep: "END_APPLICATION",
+        url:"http://quiz-user-service:3000/api/answers/dayly",
+        end: true 
+    }
 
 }
